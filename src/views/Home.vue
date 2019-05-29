2af4114fc6615b2284cde06ff8b598d4e746b8f9
amenzel <adrian.menzel@voltstorage.com>
2019-05-29 20:52:24 +0200

    first basic structure

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+    <h1>Simmarith’s might-eventually-actually-happen website</h1>
+    <h3>See nav on top of the page for more content</h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  },
+};
+</script>
+
+<style lang="less" scoped>
+img {
+  width: 700px;
+}
+</style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="projects">
+    <h1>Some Projects I did:</h1>
+  </div>
+</template>

96acfbb6219aa4876c191b5180ad7cfaf357b22d
Simmarith <menzeladrian@googlemail.com>
2019-11-18 15:58:11 +0100

    Added secret site

---
+++ src/views/Schmierwurst.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>IS MIR WURST</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Schmierwurst'
+}
+</script>
+
+<style lang="less" scoped>
+h1 {
+  font-size: 10vw;
+}
+</style>
